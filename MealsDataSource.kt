b8fd87d0c1bd70937add75c2eb976b7dd7d10039
Jacques-lang <156355612+Jacques-lang@users.noreply.github.com>
2024-08-05 21:28:42 -0600

    Create MealsDataSource.kt
    sort of a database to hold the meals that are going to be used

---
+++ MealsDataSource.kt
@@ -0,0 +1,23 @@
+package com.example.restaurantapp.data
+
+object Entree {
+    val entree= listOf(
+        Triple("Cauliflower","Whole cauliflower, brined, roasted, and deep fried",7.00),
+        Triple("Three Bean Chilli","Black beans, red beans, kidney beans, slow cooked, topped with onion",4.00),
+        Triple("Mushroom Pasta","Penned pasta, mushrooms, basil, with plum tomatoes cooked in garlic and olive oil",5.50),
+        Triple("Spicy Black Bean Skillet","Seasonal vegetables, black beans, house spice blend, served with avocado and quick pickled onions",5.50)
+    )
+    val side= listOf(
+        Triple("Summer Salad","Heriloom tomatoes, butter, lettuce, peaches, avocado, balsamic dressing" ,2.50),
+        Triple("Butternut Squash Soup","Roasted butternut squash,roasted peppers, chilli oil",3.00),
+        Triple("Spicy Potatoes","Marble potatoes, roasted, and fired in house spice blend",2.00),
+        Triple("Coconut Rice","Rice, coconut milk, lime and sugar",1.50)
+    )
+    val accomp= listOf(
+        Triple("Lunch Roll","Fresh baked roll made in house",0.50),
+        Triple("Mixed Berries","Strawberries, blueberries, and huckberries",1.00),
+        Triple("Pickled Veggies","Pickled cucumbers and carrot, made in house",0.50),
+
+
+        )
+}
